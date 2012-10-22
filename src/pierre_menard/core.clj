b34f9d81f1b72957bba8c61f30df0f734ca72cb6
Hakan Raberg <hraberg@yahoo.se>
2012-10-22 23:33:43 +0100

    Vectors

--- src/pierre_menard/core.clj
+++ src/pierre_menard/core.clj
@@ -5,7 +5,8 @@
 
 (def ^:dynamic *dimensions* 10000)
 
-(defn bsc [] (vec (repeatedly 10000 #(rand-int 2))))
+(defn bsc [] (vec (repeatedly *dimensions* #(rand-int 2))))
+
 (defn bind [x y]
   (vec (map bit-xor x y)))
 (def unbind bind)
@@ -19,4 +20,5 @@
          (apply map +)
          (map #(cond (< c %) 0
                      (> c %) 1
-                     :else (rand-int 2))))))
+                     :else (rand-int 2)))
+         vec)))
