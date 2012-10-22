18b6d6378fbf9e62bc57bb2d91294d677e1c2b9b
Hakan Raberg <hraberg@yahoo.se>
2012-10-22 23:24:23 +0100

    Faster using Clojure Vectors

--- src/pierre_menard/core.clj
+++ src/pierre_menard/core.clj
@@ -3,37 +3,20 @@
   (:import [java.math BigInteger]
            [java.util Random]))
 
-(def ^:private rnd (Random.))
 (def ^:dynamic *dimensions* 10000)
 
-(defn bsc [] (BigInteger. *dimensions* rnd))
-
-(defn binary [x]
-  (let [s (.toString x 2)]
-    (str (apply str (repeat (- *dimensions* (count s)) 0)) s)))
-
-(defn bind [x y] (.xor x y))
+(defn bsc [] (vec (repeatedly 10000 #(rand-int 2))))
+(defn bind [x y]
+  (vec (map bit-xor x y)))
 (def unbind bind)
 
-(defn ^:private bsc-vec [^BigInteger x]
-  (loop [idx 0 acc []]
-    (if (= idx *dimensions*)
-      acc
-      (recur (inc idx) (assoc acc idx (if (.testBit x idx) 1 0))))))
-
-(defn ^:private bundle-vec [^BigInteger x]
-  (loop [idx 0 acc []]
-    (if (= idx *dimensions*)
-      acc
-      (recur (inc idx) (assoc acc idx (if (.testBit x idx) 1 0))))))
+(defn binary [x] (apply str x))
 
 (defn bundle [x y & zs]
   (let [vs (concat [x y] zs)
-        c (/ (count vs) 2)
-        v (->> vs
-               (map bsc-vec)
-               (apply map +)
-               (map #(cond (< c %) 0
-                           (> c %) 1
-                           :else (rand-int 2))))]
-    (BigInteger. (apply str v) 2)))
+        c (/ (count vs) 2)]
+    (->> vs
+         (apply map +)
+         (map #(cond (< c %) 0
+                     (> c %) 1
+                     :else (rand-int 2))))))
